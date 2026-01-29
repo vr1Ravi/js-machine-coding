c7d07ef07e109674b2a7b62924e9097514a85789
Ravi Jha <ravikumarjha059@gmail.com>
2026-01-29 22:05:37 +0530

    piping in js

---
+++ Js/piping.js
@@ -0,0 +1,47 @@
+/**
+ * Input
+ * {
+  a : {
+    b : (a,b,c) => a+b+c,
+    c : (a,b,c) => a+b-c,
+  },
+  d : (a,b,c) => a-b-c
+}
+ */
+const obj =  {
+  a : {
+    b : (a,b,c) => a+b+c,
+    c : (a,b,c) => a+b-c,
+  },
+  d : (a,b,c) => a-b-c
+}
+
+
+function Fn(obj){
+    return function(...args){
+        for(let key in obj){
+            const val = obj[key];
+            if(typeof val === "function"){
+                const funArgLen = val.length;
+                const evalData = val(...args.slice(0, funArgLen));
+                obj[key] = evalData;
+            }else{
+                 Fn(val)(...args);
+            }
+        }
+    }
+}
+
+Fn(obj)(1,1,1);
+console.log(obj);
+
+/**
+ * Output:
+{
+  a : {
+    b : 3,
+    c : 1
+  },
+  d: -1
+}
+ */
