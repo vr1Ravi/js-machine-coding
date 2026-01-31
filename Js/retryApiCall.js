01515cd799ffeb8805da81895897bd5e037b2f12
Ravi Jha <ravikumarjha059@gmail.com>
2026-01-31 22:31:44 +0530

    fix retry issue

--- Js/retryApiCall.js
+++ Js/retryApiCall.js
@@ -1,49 +1,46 @@
-async function retryCall(fn, retires){
+async function retryCall(fn, retries) {
+  try {
+    const resposne = await fn();
+    return resposne;
+  } catch (error) {
+    if (retries === 1) return error;
+    return retryCall(fn, retries - 1);
+  }
+}
+
+function fetchResponse() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const random = Math.round(Math.random());
+      if (random) {
+        res({ data: "data" });
+      } else {
+        rej(new Error("failed"));
+      }
+    }, 2000);
+  });
+}
+async function callApi() {
+  try {
+    const response = await fetchResponse();
+    if (response.data) return response.data;
+  } catch (error) {
     try {
-        const resposne = await fn();  
-        return resposne;
+      const response = await retryCall(fetchResponse, 5);
+      if (response.data) return response.data;
     } catch (error) {
-        if(retires === 1) return error;
-        return retryCall(fn, retires - 1)
+      return error;
     }
+  }
 }
 
-
-function fetchResponse(){
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            const random = Math.round(Math.random());
-            if(random){
-                res({data : "data"});
-            }else{
-                rej(new Error("failed"));
-            }
-        },2000)
-    })
-}
-async function callApi(){
-     try {
-        const response = await fetchResponse();      
-        if(response.data) return  response.data
-     } catch (error) {
-        try {
-            const response = await retryCall(fetchResponse, 5);
-            if(response.data) return  response.data
-        } catch (error) {
-            return error
-        }
-     }
-}
-
-async function fetchData(){
-   try {
+async function fetchData() {
+  try {
     const data = await callApi();
     console.log("46", data);
-   } catch (error) {
+  } catch (error) {
     console.log(error);
-    
-   }
+  }
 }
 
 fetchData();
-
--- polyfills/GetElByClass/index.js
+++ polyfills/GetElByClass/index.js
@@ -1,14 +1,19 @@
 const root = document.getElementById("root");
 
-const getElByClassName = (root, className) => {
-  let ansList = [];
-  if (root.classList.contains(className)) ansList.push(root);
-  const children = Array.from(root.children);
-  for (let child of children) {
-    const childAnsList = getElByClassName(child, className);
-    ansList = [...ansList, ...childAnsList];
-  }
-  return ansList;
+// const getElByClassName = (root, className) => {
+//   let ansList = [];
+//   if (root.classList.contains(className)) ansList.push(root);
+//   const children = Array.from(root.children);
+//   for (let child of children) {
+//     const childAnsList = getElByClassName(child, className);
+//     ansList = [...ansList, ...childAnsList];
+//   }
+//   return ansList;
+// };
+
+const getElByClassNameByHierarchy = (root, hirerarchy) => {
+  const hirerarchyList = hirerarchy.split(">");
 };
 
-console.log(getElByClassName(root, "d"));
+// console.log(getElByClassName(root, "d"));
+console.log(getElByClassName(root, "a>d"));
