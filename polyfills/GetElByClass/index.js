83fdb8f669bf4d370040d023e013f6cf2b57a49c
Ravi Jha <ravikumarjha059@gmail.com>
2026-02-02 22:22:13 +0530

    class name by heirarchy

--- polyfills/GetElByClass/index.js
+++ polyfills/GetElByClass/index.js
@@ -11,9 +11,30 @@ const root = document.getElementById("root");
 //   return ansList;
 // };
 
-const getElByClassNameByHierarchy = (root, hirerarchy) => {
-  const hirerarchyList = hirerarchy.split(">");
-};
+// const getElByClassNameByHierarchy = (root, hirerarchy) => {
+//   const hirerarchyList = hirerarchy.split(">");
+//   const result = [];
+//   traverseDom(root, hirerarchyList, 0, result);
+//   return result;
+
+// };
+
+// const traverseDom = (element, hirerarchyList, index, result) => {
+//   const className = hirerarchyList[index];
+//   if(index === hirerarchyList.length - 1 && element.classList.contains(className)){
+//     result.push(element);
+//     return;
+//   }
+
+//   for(let child of element.children){
+//     if(element.classList.contains(className)){
+//       traverseDom(child, hirerarchyList, index + 1, result)
+//     }else{
+//       traverseDom(child, hirerarchyList, 0, result)
+//     }
+//   }
+
+// };
 
 // console.log(getElByClassName(root, "d"));
 console.log(getElByClassName(root, "a>d"));
