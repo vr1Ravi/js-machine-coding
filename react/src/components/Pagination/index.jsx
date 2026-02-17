48e99865eba14d6a05b1d10352e2304cb801e181
Ravi Jha <ravikumarjha059@gmail.com>
2026-02-17 23:46:02 +0530

    add page size drop down

--- react/src/components/Pagination/index.jsx
+++ react/src/components/Pagination/index.jsx
@@ -5,13 +5,15 @@ const DEFAULT_PAGE = 1;
 function Pagination({
   data = [],
   renderRow,
-  pageSize = PAGE_SIZE,
+  rowPerPage = PAGE_SIZE,
   className = "",
 }) {
   const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+  const [pageSize, setPageSize] = useState(rowPerPage);
   const startIndex = (currentPage - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const visibleData = data.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(data.length / pageSize);
 
   return (
     <div className={`pagination ${className}`}>
@@ -22,10 +24,32 @@ function Pagination({
       </div>
 
       <div className="pagination-footer">
-        <button onClick={() => setCurrentPage((prev) => prev - 1)}>
+        <select onChange={(e) => setPageSize(e.target.value)}>
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={30}>30</option>
+        </select>
+        <button disabled={currentPage === 1} onClick={() => setCurrentPage(1)}>
+          First
+        </button>
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
           Previous
         </button>
-        <button onClick={() => setCurrentPage((prev) => prev + 1)}>Next</button>
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(totalPages)}
+        >
+          Last
+        </button>
       </div>
     </div>
   );
